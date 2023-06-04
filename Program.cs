2c819148d3ca9523309ad2486164f0b47920be27
Noga25 <nogale40@gmail.com>
2023-06-04 21:14:49 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
+
+namespace MySQL
+{
+    class Program
+    {
+        private MySqlConnection Connection;
+
+        static void Main(string[] args)
+        {
+            string connstring = "Server=localhost; database=game; UID=root; password=noga0000";
+            MySqlConnection connection = new MySqlConnection(connstring);
+            connection.Open();
+            Console.WriteLine(connection.State);
+
+            string query = "SELECT Name,Health FROM players";
+            var cmd = new MySqlCommand(query, connection);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                string someStringFromColumnZero = reader.GetString(0);
+                string someStringFromColumnOne = reader.GetString(1);
+                Console.WriteLine(someStringFromColumnZero + "," + someStringFromColumnOne);
+            }
+            connection.Close();
+        }
+    }
+}
